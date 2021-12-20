c43a9e768d9169092531c841690d4a2c83aa7bd7
Chathura Hewa Algiriyage <cpriyankara@ecs.vuw.ac.nz>
2021-12-21 12:56:28 +1300

    Removed the data processor.

--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -2,42 +2,65 @@ package server;
 
 import javafx.scene.control.TextArea;
 import parser.DataProcessor;
-import parser.Food;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ServerThread extends Thread {
     private Socket socket;
-    private DataProcessor dataProcessor;
     private TextArea textArea;
+    private final static String LOGIN = "LOGIN", SEND="SEND", LEAVE="LEAVE";
 
     public ServerThread(Socket s, TextArea textArea) {
         this.socket = s;
-        dataProcessor = new DataProcessor();
         this.textArea = textArea;
     }
 
     public void run() {
         //Try with resources
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             //Read client request
             String request = reader.readLine();
-            textArea.appendText("\nThe client is searching for: "+request);
-            //Get the requested food object
-            ArrayList<Food> foods = dataProcessor.findFoodByName(request);
-            //Convert food object to XML
-            String xml = dataProcessor.marshallFood(foods);
-            //Send the XML to client as the response
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(xml);
-            socket.close();
+            if(request != null) {
+                String[] req = request.split(":");
+                String command = req[0];
+                switch (command) {
+                    case LOGIN: {
+                        String username = req[1];
+                        ThreadedServer.addClientSocket(username, this.socket);
+                        sendMessage("OK");
+                        break;
+                    }
+                    case SEND:
+                        String destUsername = req[1];
+                        String message = req[2];
+                        Socket socket = ThreadedServer.getClientSocket(destUsername);
+                        sendMessage(socket, message);
+                        break;
+                    case LEAVE: {
+                        String username = req[1];
+                        ThreadedServer.removeClientSocket(username);
+                        break;
+                    }
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendMessage(String message) throws IOException {
+        PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+        out.println(message);
+    }
+
+    public void sendMessage(Socket socket1, String message) throws IOException {
+        PrintWriter out = new PrintWriter(socket1.getOutputStream(), true);
+        out.println(message);
     }
 }
